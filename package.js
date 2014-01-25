2ac0b7987d017e80ef90f64e4a51c609140d31ac
Colin Sullivan <colin@colin-sullivan.net>
2014-01-24 18:02:47 -0800

    Finished wrapping it up

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "A wrapper around the blueimp-file-upload package for Meteor."
+});
+
+Package.on_use(function (api) {
+  api.use(["jquery", "jqueryui", "blueimp-tmpl"], "client");
+
+  api.add_files([
+    "./lib/blueimp-file-upload/css/jquery.fileupload-ui.css",
+    "./lib/blueimp-file-upload/js/jquery.fileupload.js",
+    "./lib/blueimp-file-upload/js/jquery.fileupload-ui.js"
+  ], "client");
+
+});
