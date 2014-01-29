1a4863ee6f42eda1e6c671bab0edb96d0d2c5bf9
Colin Sullivan <colin@colin-sullivan.net>
2014-01-29 15:32:15 -0800

    Updated to include only the files we need

--- package.js
+++ package.js
@@ -6,9 +6,8 @@ Package.on_use(function (api) {
   api.use(["jquery", "jqueryui", "blueimp-tmpl"], "client");
 
   api.add_files([
-    "./lib/blueimp-file-upload/css/jquery.fileupload-ui.css",
-    "./lib/blueimp-file-upload/js/jquery.fileupload.js",
-    "./lib/blueimp-file-upload/js/jquery.fileupload-ui.js"
+    "./lib/blueimp-file-upload/css/jquery.fileupload.css",
+    "./lib/blueimp-file-upload/js/jquery.fileupload.js"
   ], "client");
 
 });
